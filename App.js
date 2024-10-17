c0b41a09c48b1000e71082488eb173a4ed5309bb
hjoo830 <hjoo830@naver.com>
2024-10-18 00:12:26 +0900

    💄 Design: Hide status bar

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as Linking from "expo-linking";
+import { StatusBar } from "expo-status-bar";
 
 import Payment from "./screens/Payment";
 import QRcode from "./screens/QRcode";
@@ -21,18 +22,21 @@ export default function App() {
   };
 
   return (
-    <NavigationContainer linking={linking}>
-      <Stack.Navigator
-        initialRouteName="Main"
-        screenOptions={{
-          headerShown: false, // 모든 화면의 헤더 숨기기
-        }}
-      >
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="Payment" component={Payment} />
-        <Stack.Screen name="QRcode" component={QRcode} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar hidden={true} />
+      <NavigationContainer linking={linking}>
+        <Stack.Navigator
+          initialRouteName="Main"
+          screenOptions={{
+            headerShown: false, // 모든 화면의 헤더 숨기기
+          }}
+        >
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="Payment" component={Payment} />
+          <Stack.Screen name="QRcode" component={QRcode} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
