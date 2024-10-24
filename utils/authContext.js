fa545872cf86108e591d1da95283823f0771b7ac
daj3on9 <dajung5623@naver.com>
2024-10-24 21:50:34 +0900

    ✨ Feat : add Login API

---
+++ utils/authContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useContext, useEffect } from "react";
+import {getTokenFromStorage} from "./tokenUtils";
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [isLogged, setIsLogged] = useState(false);
+
+    useEffect(() => {
+        const checkToken = async () => {
+            const token = await getTokenFromStorage();
+            setIsLogged(!!token);
+        };
+
+        checkToken();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ isLogged, setIsLogged }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
